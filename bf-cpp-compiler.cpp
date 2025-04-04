5eea29d2add199e1337626b9e31936b987d366da
Kris Taileer <191796700+Kris-Taileer@users.noreply.github.com>
2025-04-04 16:54:11 +0300

    class and main code
    basic functions (except loops)

---
+++ bf-cpp-compiler.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "class.h"
+
+using namespace std;
+
+int main() {
+    vector <Slot> slots(1000, 0);
+    string mes;
+    while (mes != "0") {
+        while (cin >> mes) {
+            if (mes == "+") {
+                slots[pos].add(pos);
+            }
+            if (mes == "-") {
+                slots[pos].less(pos);
+            }
+            if (mes == ">") {
+                slots[pos].right();
+            }
+            if (mes == "<") {
+                slots[pos].left();
+            }
+            if (mes == ".") {
+                slots[pos].dot(pos);
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ class.h
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+unsigned int pos = 0;
+
+
+void pos_left(int pos) {
+    if (pos == 0) cout << "Negative mem index\n";
+    if (pos != 0) pos--;
+}
+
+void pos_right(int pos) {
+    pos++;
+}
+
+class Slot {
+private:
+    int var;
+    int i;
+public:
+    Slot(int _var = 0): var(_var) {}
+
+    void add(int i) {
+        var++;
+    }
+
+    void less(int i) {
+        var--;
+    }
+
+    void dot(int i)  {
+        cout << var << endl;
+    }
+
+    void right()  {
+        pos_right(pos);
+    }
+
+    void left() {
+        pos_left(pos);
+    }
+
+};
